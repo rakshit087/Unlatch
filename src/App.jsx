cc93d37a31afbcb625c8d827905cb4f90b4135ed
Rakshit <rakshit087@gmail.com>
2022-04-11 23:47:39 +0530

    Added Theme 💅

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./utilities/theme";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">Hello World</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/utilities/theme.js
@@ -0,0 +1,21 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+const palette = {
+  type: "light",
+  primary: { main: "#45B3F2" },
+  secondary: { main: "#7F54B3" },
+  text: { primary: "#645D5D", secondary: "#B1B1B1", disabled: "#B1B1B1" },
+};
+
+const typography = {
+  fontFamily: `sans-serif`,
+  fontSize: 16,
+  fontWeightLight: 300,
+  fontWeightRegular: 400,
+  fontWeightMedium: 500,
+};
+
+const themeName = "Unlash Light";
+let theme = createTheme({ palette: palette, typography: typography, themeName });
+theme = responsiveFontSizes(theme);
+export default theme;
