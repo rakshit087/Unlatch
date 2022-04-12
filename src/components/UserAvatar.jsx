c83c882cdbe498e9b6c5e948acb77fb5bc0c1d93
Rakshit <rakshit087@gmail.com>
2022-04-13 00:51:28 +0530

    Added primary color to avatar

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -2,7 +2,7 @@ import { Avatar } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 const UserAvatar = () => {
   return (
-    <Avatar sx={{ bgcolor: "primary" }}>
+    <Avatar sx={{ bgcolor: "primary.main" }}>
       <PersonIcon />
     </Avatar>
   );
