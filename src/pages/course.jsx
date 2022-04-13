0e534b63bb4b0c47126b8716a4db83a74dc4e0d5
Rakshit <rakshit087@gmail.com>
2022-04-13 13:20:45 +0530

    created course page (with player bitch)

--- src/pages/course.jsx
+++ src/pages/course.jsx
@@ -4,14 +4,12 @@ import { ContractService } from "../utilities/contract";
 import { WalletServices } from "../utilities/wallet";
 import { Navbar } from "../layouts/Navbar";
 import { Box, Typography } from "@mui/material";
+import ReactPlayer from "react-player";
 
 const Course = () => {
   const router = useRouter();
   const [videos, setVideos] = useState([]);
-  const [currentlyPlaying, setCurrentlyPlaying] = useState({
-    name: "",
-    cid: "",
-  });
+  const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
   useEffect(() => {
     if (!window.ethereum) {
       alert("Please install MetaMask");
@@ -26,25 +24,38 @@ const Course = () => {
           const id = parseInt(router.query.id);
           console.log(id);
           ContractService.getVideos(id).then((v) => {
+            console.log(v);
             setVideos(v);
             setCurrentlyPlaying(v[0]);
           });
         }
       });
     }
-  }, []);
-  return (
-    <div>
-      <Navbar />
-      <Box sx={{ px: { xs: 1, md: 10, lg: 22 }, py: 5 }}>
-        <Box>
-          <Typography variant="h4" marginBottom={8} noWrap>
-            {currentlyPlaying.name}
-          </Typography>
+  }, [router.isReady]);
+  if (currentlyPlaying === null) {
+    return (
+      <div>
+        <Navbar />
+        <Box display="flex" justifyContent="center" alignItems="center" height="75vh">
+          <Typography variant="h4">Loading...</Typography>
         </Box>
-      </Box>
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Navbar />
+        <Box sx={{ px: { xs: 1, md: 10, lg: 22 }, py: 5 }}>
+          <Box>
+            <Typography variant="h4" marginBottom={8} noWrap>
+              {currentlyPlaying.name}
+            </Typography>
+            <ReactPlayer url={"https://crustwebsites.net/ipfs/" + currentlyPlaying.cid} controls />
+          </Box>
+        </Box>
+      </div>
+    );
+  }
 };
 
 export default Course;
